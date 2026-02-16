8817406a37dcfeafca42a2c7d9fe552b0e21247d
Professional_Mario <mani1756067@gmail.com>
2026-02-16 22:01:15 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+Hypen_e_dot= '-e .'
+def get_requirements(file_path:str)->List[str]:
+    #This function will return the list of requirements
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements= file_obj.readlines()
+        [req.replace ("\n","") for req in requirements]
+        
+        if Hypen_e_dot in requirements:
+            requirements.remove(Hypen_e_dot)
+
+
+
+setup(
+    name='Mini_Project',
+    version='0.0.1',
+    Author= 'Manikandan',
+    Auther_mail= 'mani1756067@gmail.com',
+    packages= find_packages(),
+    install_requires= get_requirements('requirements.txt')
+    )
+
