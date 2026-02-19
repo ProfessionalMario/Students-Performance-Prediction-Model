b84c703eacddad3e3175e4780781a7facb89034a
Professional_Mario <mani1756067@gmail.com>
2026-02-19 17:55:34 +0530

    Model Trainer

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -9,7 +9,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.Components.data_transformation import DataTransformation
 from src.Components.data_transformation import DataTransformationConfig
-
+from src.Components.model_trainer import ModelTrainer
+from src.Components.model_trainer import ModelTrainerConfig
 #Importing our data 
 
 @dataclass
@@ -56,4 +57,18 @@ if __name__=="__main__":
 
 
     data_transformation= DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, preprocessor_path = data_transformation.initiate_data_transformation(train_data, test_data)
+
+
+    modeltrainer= ModelTrainer()
+    
+    r2 = modeltrainer.initiate_model_trainer(
+    train_arr,
+    test_arr,
+    preprocessor_path)
+    print("Final R2 Score:", r2)
+
+
+
+
+
