906ab8d6d9e44ff2efb74853671dfcb2e2f8716a
Professional_Mario <mani1756067@gmail.com>
2026-02-19 13:56:00 +0530

    with pickle

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,8 @@
 import sys
-from logger  import logging
+from src.logger  import logging
 
 def error_message_detail(error, error_detail):
-    _, _, exc_tb = error_detail
+    _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = (
         f"Error occurred in python script name [{file_name}] "
@@ -12,14 +12,15 @@ def error_message_detail(error, error_detail):
     return error_message
 
 
+
 class CustomException(Exception):
-    def __init__(self, error_message):
-        _, _, exc_tb = sys.exc_info()
-        self.error_message = error_message_detail(error_message, sys.exc_info())
-        super().__init__(self.error_message)
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
         return self.error_message
+
 
 
 if __name__ == "__main__":
